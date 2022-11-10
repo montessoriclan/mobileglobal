4437221f4269f08c79d5f9328a09227c3e00a273
bibouh <messanfollyh@gmail.com>
2022-11-10 12:12:41 +0100

    connectivity

--- lib/page/auth_page/login.dart
+++ lib/page/auth_page/login.dart
@@ -9,6 +9,7 @@ import 'package:deernier/widget/k_form_field.dart';
 import 'package:deernier/widget/k_height.dart';
 import 'package:deernier/widget/k_password_field.dart';
 import 'package:deernier/widget/k_text.dart';
+import 'package:deernier/widget/no_network.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
@@ -26,86 +27,91 @@ class _LoginViewState extends State<LoginView> {
   bool? isLoading = false;
 
   Future<void> login() async {
-     showDialog(
-      barrierDismissible: false,
-      context: context, builder: (conext)=>
-      AlertDialog(
-        
-      title: Column(children: const <Widget> [
-        Text("please wait ..."),
-        KHeight(height: 10),
-        LinearProgressIndicator(),
-      ]),
-    ));
+    showDialog(
+        barrierDismissible: false,
+        context: context,
+        builder: (conext) => AlertDialog(
+              title: Column(children: const <Widget>[
+                Text("please wait ..."),
+                KHeight(height: 10),
+                LinearProgressIndicator(),
+              ]),
+            ));
     try {
       await AuthService().signInWithEmailAndPassword(LoginModel(
           email: textEmailController.text,
           password: textPasswordController.text));
-            navigatorKey.currentState!.popUntil((route)=>route.isFirst);
-          // ignore: use_build_context_synchronously
-          Navigator.pushReplacement(
-                    context,
-                    MaterialPageRoute(
-                      builder: (context) => const Profil(),
-                    ));
+      navigatorKey.currentState!.popUntil((route) => route.isFirst);
+      // ignore: use_build_context_synchronously
+      Navigator.pushReplacement(
+          context,
+          MaterialPageRoute(
+            builder: (context) => const Profil(),
+          ));
     } on FirebaseAuthException catch (e) {
-       
       setState(() {
         errorMessage = e.message;
       });
       ScaffoldMessenger.of(context)
           .showSnackBar(SnackBar(content: Text(errorMessage.toString())));
     }
-     navigatorKey.currentState!.popUntil((route)=>route.isFirst);
-    
+    navigatorKey.currentState!.popUntil((route) => route.isFirst);
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SafeArea(
-          child: SingleChildScrollView(
-        child: Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 25),
-          child: Column(children: <Widget>[
-            Row(
-              children: const <Widget> [
-                 KText(text: "login",size: AppConstant.leadingText,weight: FontWeight.bold),
-              ],
-            ),
-            const KHeight(height: 20),
-             Row(
-              children: const <Widget> [
-                 KText(text: "login to access on your account",size: AppConstant.titleText,weight: FontWeight.w500),
-              ],
-            ),
-                const KHeight(height: 40),
-            KFormField(
-                controller: textEmailController,
-                hint: "Enter your email",
-                type: TextInputType.emailAddress),
-            const KHeight(height: 10),
-            KPasswordField(
-                text: "password",
-                type: TextInputType.text,
-                textcontroler: textPasswordController),
-            const KHeight(height: 20),
-            KButtonPrimary(text: "Login", function: login),
-            const KHeight(height: 10),
-            Column(
-              children: const [ Text("or")],
-            ),
-            const KHeight(height: 10),
-            KButtonPrimary(
-                text: "create a new account",
-                function: () => Navigator.push(
-                    context,
-                    MaterialPageRoute(
-                      builder: (context) => const RegisterView(),
-                    ))),
-          ]),
-        ),
-      )),
+      body: NoNetwork(
+        child: SafeArea(
+            child: SingleChildScrollView(
+          child: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 25),
+            child: Column(children: <Widget>[
+              Row(
+                children: const <Widget>[
+                  KText(
+                      text: "login",
+                      size: AppConstant.leadingText,
+                      weight: FontWeight.bold),
+                ],
+              ),
+              const KHeight(height: 20),
+              Row(
+                children: const <Widget>[
+                  KText(
+                      text: "login to access on your account",
+                      size: AppConstant.titleText,
+                      weight: FontWeight.w500),
+                ],
+              ),
+              const KHeight(height: 40),
+              KFormField(
+                  controller: textEmailController,
+                  hint: "Enter your email",
+                  type: TextInputType.emailAddress),
+              const KHeight(height: 10),
+              KPasswordField(
+                  text: "password",
+                  type: TextInputType.text,
+                  textcontroler: textPasswordController),
+              const KHeight(height: 20),
+              KButtonPrimary(text: "Login", function: login),
+              const KHeight(height: 10),
+              Column(
+                children: const [Text("or")],
+              ),
+              const KHeight(height: 10),
+              KButtonPrimary(
+                  text: "create a new account",
+                  function: () => Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) => const RegisterView(),
+                      ))),
+            ]),
+          ),
+        )),
+      ),
     );
   }
 }
